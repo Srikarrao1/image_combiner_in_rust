6827f63b92ba686db3abc88c7b4182733fc654a1
Srikar <pasupuleti@antiersolutions.com>
2023-08-03 16:49:18 +0530

    first commit

---
+++ src/args.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+pub struct Args {
+    pub image_1: String,
+    pub image_2: String,
+    pub result: String,
+}
+
+impl Args {
+    pub fn new() -> Self {
+        Args {
+            image_1: get_nth_arg(1),
+            image_2: get_nth_arg(2),
+            result: get_nth_arg(3),
+        }
+    }
+}
+
+fn get_nth_arg(n: usize) -> String {
+    std::env::args().nth(n).unwrap()
+}
+
+
+
+
+
+
